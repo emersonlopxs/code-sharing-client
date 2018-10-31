7398f78ccf17ab8843d151d828570433d629fb88
lupuselit <lupuselit@outlook.com>
2018-10-31 11:58:48 -0300

    new files

---
+++ build/precache-manifest.751d0897910e99bc4bc75faeaae7eb74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da0c043242ae82f1562a",
+    "url": "/code-sharing-client-build/static/js/runtime~main.da0c0432.js"
+  },
+  {
+    "revision": "7d043c922910f3843768",
+    "url": "/code-sharing-client-build/static/js/main.7d043c92.chunk.js"
+  },
+  {
+    "revision": "ed5f83da0a52e50a698c",
+    "url": "/code-sharing-client-build/static/js/1.ed5f83da.chunk.js"
+  },
+  {
+    "revision": "7d043c922910f3843768",
+    "url": "/code-sharing-client-build/static/css/main.8da25a1c.chunk.css"
+  },
+  {
+    "revision": "ce20254a3d4d842db8ee2d8185134b3c",
+    "url": "/code-sharing-client-build/index.html"
+  }
+];
