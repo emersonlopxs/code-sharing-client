fcd2c7ba84a244c6bb2a1e202499389ada4cd5f7
lupuselit <lupuselit@outlook.com>
2018-12-18 00:11:34 -0200

    app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import GetCodes from './GetCodes';
