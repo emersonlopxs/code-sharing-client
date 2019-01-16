a29b489be73926332eeccade916706ddfae1100d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-16 22:24:33 +0000

    Deploy lupuselit/code-sharing-client to github.com/lupuselit/code-sharing-client.git:gh-pages

---
+++ precache-manifest.f61a8f2e2b41ed52ddcf89337733cc35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90dc42576539b7ce0fba",
+    "url": "/code-sharing-client/static/js/runtime~main.90dc4257.js"
+  },
+  {
+    "revision": "aad27c02df5d2629085e",
+    "url": "/code-sharing-client/static/js/main.aad27c02.chunk.js"
+  },
+  {
+    "revision": "10c9cd123984804ffe5f",
+    "url": "/code-sharing-client/static/js/1.10c9cd12.chunk.js"
+  },
+  {
+    "revision": "aad27c02df5d2629085e",
+    "url": "/code-sharing-client/static/css/main.60152ee1.chunk.css"
+  },
+  {
+    "revision": "50163ef1c1101c72ab7db4fbac03d6c6",
+    "url": "/code-sharing-client/index.html"
+  }
+];
